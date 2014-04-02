48966187ba8f0f22540579e172617992bad60db2
planemad <arun.planemad@gmail.com>
2014-04-03 00:49:34 +0530

    Path generation working

--- make/make-atlas.py
+++ make/make-atlas.py
@@ -1,21 +1,49 @@
 #!/usr/bin/env python
 #Makes wikimaps atlas database
 
+# Database settings
+host = "localhost"
+port = 5432
+user = "postgres"
+
 # Install dependencies: sudo easy_install psycopg2 pyyaml
 import subprocess	# for making system calls
-import psycopg2 	# for communicating with postgres 
-import yaml		# for reading yaml config file
+import psycopg2     # for communicating with postgres 
+import yaml         # for reading yaml config file
 
+def atlas_create():
+    "Creates fresh databases"
+    
+    # Create atlas db
+    subprocess.call("psql -U postgres -c \"drop database atlas;\"", shell=True)
+    subprocess.call("psql -U postgres -c \"create database atlas;\"", shell=True)
 
-# Create atlas db
-subprocess.call("psql -U postgres -c \"drop database atlas;\"", shell=True)
-subprocess.call("psql -U postgres -c \"create database atlas;\"", shell=True)
+    # Create datasource db
+    subprocess.call("psql -U postgres -c \"drop database ne;\"", shell=True)
+    subprocess.call("psql -U postgres -c \"create database ne;\"", shell=True)
+    subprocess.call("psql -U postgres -d ne -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql", shell=True)
+    subprocess.call("psql -U postgres -d ne -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql", shell=True)
+    return
 
-# Create ne db
-subprocess.call("psql -U postgres -c \"drop database ne;\"", shell=True)
-subprocess.call("psql -U postgres -c \"create database ne;\"", shell=True)
-subprocess.call("psql -U postgres -d ne -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql", shell=True)
-subprocess.call("psql -U postgres -d ne -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql", shell=True)
+#atlas_create()
+
+def data_create():
+    
+    with open('atlas-data.yaml', 'r') as f:
+        "Load atlas data configuration"
+        atlas_data = yaml.load(f)
+        
+    for datasource in atlas_data["datasource"]:
+        for layer in datasource["layer"]:
+            path = atlas_data["path"]+datasource["path"]+layer["path"]
+            print path
+            
+                
+           
+            
+    
+    
+data_create()
 
 # Connect to atlas
 atlas = psycopg2.connect("dbname=atlas user=postgres")
