78c210f59334ce92f9ebbf4ca65068c4fad7b5ed
planemad <arun.planemad@gmail.com>
2014-11-16 14:23:18 +0530

    Serve topojson

--- server/app.py
+++ server/app.py
@@ -1,22 +1,12 @@
-# Wikiatlas API
-# /api/v1
-
-# METHODS
-# GET
-# /world List available countries with 2 letter ISO code
-# /world/<country_name|iso_a2> List administrative subunuts within country with hasc codes
-# /bbox/<country_name|iso_a2|hasc> Return bounding box of country name or iso or hasc code
-# /iso2/<country_name|iso3> Return ISO_a2 from hasc or iso_a3 code or country name
-# /topojson/<iso_a2|hasc>/adm|natural|highway|railway|place|waterway Return topojson data for requested admin area
-
 # REFERENCE
 # http://flask.pocoo.org/docs/0.10/quickstart/#a-minimal-application
 # https://pythonhosted.org/Flask-SQLAlchemy/
 # http://zetcode.com/db/postgresqlpythontutorial/
 # http://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
+# http://blog.luisrei.com/articles/flaskrest.html
 
 # DEPENDENCIES
-#sudo pip install flask
+#sudo pip install flask flask-restful
 # pip install -U psycopg2
 
 # Install dependencies: sudo easy_install psycopg2 pyyaml
@@ -25,7 +15,13 @@ import psycopg2     # for communicating with postgres
 import psycopg2.extras
 import yaml         # for reading yaml config file
 import json
+import textwrap
 
+from flask import Flask, jsonify, make_response
+from flask.ext.restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
 
 # Local settings
 import local_settings
@@ -60,10 +56,22 @@ def psql_sql(sql_file):
     bash(psql_sql)
     return
 
-#!flask/bin/python
-from flask import Flask, jsonify, make_response
+# API Index
+@app.route('/api/v1/')
+def api_root():
+    text = '''
+    <h1>Wikiatlas API</h1>
+    <h2>api/v1</h2>
 
-app = Flask(__name__)
+    <ul>
+    <li> <pre>/world</pre> List available countries with 2 letter ISO code</li>
+    <li> <pre>/world/<country_name|iso_a2></pre> List administrative subunuts within country with hasc codes</li>
+    <li> <pre>/bbox/<country_name|iso_a2|hasc></pre> Return bounding box of country name or iso or hasc code</li>
+    <li> <pre>/iso2/<country_name|iso3></pre> Return ISO_a2 from hasc or iso_a3 code or country name</li>
+    <li> <pre>/topojson/<iso_a2|hasc>/adm|natural|highway|railway|place|waterway</pre> Return topojson data for requested admin area</li>
+    </ul>
+    '''
+    return textwrap.dedent(text).strip()
 
 # Return list of adm0 areas
 @app.route('/api/v1/world/', methods=['GET'])
@@ -103,16 +111,11 @@ def generate_adm_bbox(adm_area):
     atlas.close()
     return json.dumps(countries)
 
-#def atlas_query_by_code
-
-# Return topojson data of requested area
-@app.route('/api/v1/topojson/<path:adm_area>', methods=['GET'])
-def generate_topojson(adm_area):
+# Return geojson data of requested area
+@app.route('/api/v1/geojson/<path:adm_area>', methods=['GET'])
+def generate_geojson(adm_area):
     atlas = psycopg2.connect(psycopg_connect_atlas)
     atlas_cur = atlas.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    
-    
-#        countries = atlas_query_by_code("ST_Box2D(the_geom)",hasc_code)
 
     ## If adm_area is a hasc code, lookup the adm1 table
     if "." in adm_area :
@@ -122,6 +125,13 @@ def generate_topojson(adm_area):
     countries = atlas_cur.fetchall()
     atlas.close()
     return json.dumps(countries)
+
+# Return topojson data of requested area
+@app.route('/api/v1/topojson/<path:adm_area>', methods=['GET'])
+def generate_topojson(adm_area):
+    bash("python ../postgis2geojson/postgis2geojson.py -d wikimaps_atlas -t adm0_area -u postgres -g the_geom --topojson")
+    with open('data.geojson', 'r') as f:
+        return json.dumps(json.load(f))
 
 # 404 Error handler
 @app.errorhandler(404)
